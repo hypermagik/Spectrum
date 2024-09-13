ef659af3eb8ae447efe0af47e8b0fc47efc2d68c
Sorin Popescu <popirop@gmail.com>
2024-09-13 11:13:48 +0300

    Add frequency shifter

---
+++ lib/src/main/java/com/hypermagik/spectrum/lib/dsp/Shifter.kt
@@ -0,0 +1,29 @@
+package com.hypermagik.spectrum.lib.dsp
+
+import com.hypermagik.spectrum.lib.data.Complex32Array
+import kotlin.math.PI
+import kotlin.math.cos
+import kotlin.math.sin
+
+class Shifter(sampleRate: Int, frequency: Long) {
+    private var omega = (2 * PI * frequency / sampleRate).toFloat()
+    private var phi = 0.0f
+
+    fun shift(input: Complex32Array, output: Complex32Array, length: Int = input.size) {
+        if (input.size != output.size) {
+            throw RuntimeException("Input and output arrays must have the same size")
+        }
+
+        for (i in 0 until length) {
+            output[i].setmul(input[i], cos(phi), sin(phi))
+
+            phi += omega
+
+            if (phi >= PI * 2) {
+                phi = (phi - 2 * PI).toFloat()
+            } else if (phi < 0) {
+                phi = (phi + 2 * PI).toFloat()
+            }
+        }
+    }
+}
