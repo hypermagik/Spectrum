2edcc8e48265bf03ec989e6f4bebaa74a4a518d1
Sorin Popescu <popirop@gmail.com>
2024-09-17 11:36:34 +0300

    Add more window coefficients

--- lib/src/main/java/com/hypermagik/spectrum/lib/dsp/Window.kt
+++ lib/src/main/java/com/hypermagik/spectrum/lib/dsp/Window.kt
@@ -6,29 +6,29 @@ import kotlin.math.cos
 import kotlin.math.min
 
 class Window {
-    enum class Type { HAMMING, BLACKMAN_HARRIS, FLAT_TOP }
+    enum class Type { BLACKMAN, BLACKMAN_HARRIS, BLACKMAN_NUTALL, FLAT_TOP, HAMMING, HANN, RECTANGULAR }
 
     companion object {
+        private val coefficients = mapOf(
+            Type.BLACKMAN to floatArrayOf(0.42f, 0.5f, 0.08f),
+            Type.BLACKMAN_HARRIS to floatArrayOf(0.35875f, 0.48829f, 0.14128f, 0.01168f),
+            Type.BLACKMAN_NUTALL to floatArrayOf(0.3635819f, 0.4891775f, 0.1365995f, 0.0106411f),
+            Type.FLAT_TOP to floatArrayOf(1.0f, 1.930f, 1.290f, 0.388f, 0.028f),
+            Type.HAMMING to floatArrayOf(0.54f, 0.46f),
+            Type.HANN to floatArrayOf(0.5f, 0.5f),
+            Type.RECTANGULAR to floatArrayOf(1.0f),
+        )
+
         fun make(type: Type, size: Int): FloatArray {
+            val coefficients = coefficients[type]!!
             val window = FloatArray(size)
 
             for (i in window.indices) {
-                val w = when (type) {
-                    Type.HAMMING -> 0.54f - 0.46f * cos(2 * PI * i / (size - 1))
-
-                    Type.BLACKMAN_HARRIS -> 0.35875f -
-                            0.48829f * cos(2 * PI * i / (size - 1)) +
-                            0.14128f * cos(4 * PI * i / (size - 1)) -
-                            0.01168f * cos(6 * PI * i / (size - 1))
-
-                    Type.FLAT_TOP -> 1.0f -
-                            1.930f * cos(2 * PI * i / (size - 1)) +
-                            1.290f * cos(4 * PI * i / (size - 1)) -
-                            0.388f * cos(6 * PI * i / (size - 1)) +
-                            0.028f * cos(8 * PI * i / (size - 1))
+                var sign = 1.0f
+                for (j in coefficients.indices) {
+                    window[i] += sign * coefficients[j] * cos(j * 2 * PI * i / (size -1)).toFloat()
+                    sign = -sign
                 }
-
-                window[i] = w.toFloat()
             }
 
             return window
