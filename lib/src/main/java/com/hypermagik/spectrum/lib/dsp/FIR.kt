992959c8bfa1bdf72d754a708a48387ec34ed9dc
Sorin Popescu <popirop@gmail.com>
2024-09-21 17:23:47 +0300

    Make FIR filter almost two times faster

--- lib/src/main/java/com/hypermagik/spectrum/lib/dsp/FIR.kt
+++ lib/src/main/java/com/hypermagik/spectrum/lib/dsp/FIR.kt
@@ -4,55 +4,51 @@ import com.hypermagik.spectrum.lib.data.Complex32
 import com.hypermagik.spectrum.lib.data.Complex32Array
 
 class FIR(private val taps: FloatArray, private val decimation: Int = 1, private val half: Boolean = false) {
-    private var delayLine = Complex32Array(taps.size) { Complex32() }
-
-    private var tapIndex = 0
+    private var buffer = Complex32Array(0) { Complex32() }
     private var decimationCounter = 1
 
     fun filter(input: Complex32Array, output: Complex32Array, length: Int = input.size): Int {
-        var j = 0
+        if (buffer.size < length + taps.size) {
+            buffer = Complex32Array(length + taps.size) { Complex32() }
+        }
 
         for (i in 0 until length) {
-            delayLine[tapIndex].set(input[i])
+            buffer[taps.size - 1 + i].set(input[i])
+        }
 
+        var outputIndex = 0
+
+        for (i in 0 until length) {
             if (decimationCounter == 0) {
-                if (j == output.size) {
-                    return output.size
-                }
+                val out = output[outputIndex++]
 
-                output[j].zero()
+                out.zero()
 
                 if (half) {
                     val middle = taps.size / 2
 
-                    var index = (tapIndex - middle).mod(taps.size)
-                    output[j].addmul(delayLine[index], taps[middle])
+                    out.addmul(buffer[i + middle], taps[middle])
 
-                    var k = 1
-                    while (k < middle) {
-                        index = (tapIndex - middle - k).mod(taps.size)
-                        output[j].addmul(delayLine[index], taps[middle + k])
-
-                        index = (tapIndex - middle + k).mod(taps.size)
-                        output[j].addmul(delayLine[index], taps[middle - k])
-
-                        k += 2
+                    var j = 1
+                    while (j < middle) {
+                        out.addmul(buffer[i + middle + j], taps[middle + j])
+                        out.addmul(buffer[i + middle - j], taps[middle - j])
+                        j += 2
                     }
                 } else {
-                    var index = tapIndex
-                    for (tap in taps) {
-                        output[j].addmul(delayLine[index], tap)
-                        index = (index - 1).mod(taps.size)
+                    for (j in taps.indices) {
+                        out.addmul(buffer[i + j], taps[j])
                     }
                 }
-
-                j++
             }
 
-            tapIndex = (tapIndex + 1) % taps.size
             decimationCounter = (decimationCounter + 1) % decimation
         }
 
-        return j
+        for (i in 0 until taps.size - 1) {
+            buffer[i].set(buffer[length + i])
+        }
+
+        return outputIndex
     }
 }
